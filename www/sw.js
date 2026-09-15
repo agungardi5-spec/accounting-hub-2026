ed6109170c8ca376184c1741d3ca4a6b102f4e22
agungardi5-spec <agungardi5@gmail.com>
2026-09-15 16:52:37 +0700

    Upgrade service worker to version 2 with enhancements
    Updated service worker to version 2, added new icons and improved cache management.

--- www/sw.js
+++ www/sw.js
@@ -1,4 +1,42 @@
-const CACHE='accounting-hub-v1';
-const ASSETS=['./','./index.html','./manifest.json','./data/accounting.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const y=x.clone();caches.open(CACHE).then(c=>c.put(e.request,y));return x}).catch(()=>caches.match('./index.html')))));
+const CACHE = 'accounting-hub-v2';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './data/accounting.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(k => k !== CACHE)
+          .map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return response;
+      });
+    }).catch(() => caches.match('./index.html'))
+  );
+});
