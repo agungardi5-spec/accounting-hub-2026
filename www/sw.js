1ebdb7880a932cc21d62a43a15c7f4cfad7aba96
agungardi5-spec <agungardi5@gmail.com>
2026-09-15 17:32:03 +0700

    Update service worker to version 3

--- www/sw.js
+++ www/sw.js
@@ -1,42 +1,5 @@
-const CACHE = 'accounting-hub-v2';
-
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './data/accounting.json',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(k => k !== CACHE)
-          .map(k => caches.delete(k))
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => {
-      return cached || fetch(e.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(c => c.put(e.request, copy));
-        return response;
-      });
-    }).catch(() => caches.match('./index.html'))
-  );
-});
+const CACHE='accounting-hub-v3';
+const ASSETS=['./','./index.html','./manifest.json','./data/accounting.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match('./index.html'))))});
