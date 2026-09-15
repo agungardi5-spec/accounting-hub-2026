e0bc8c15a18877c33f1b9c446b2b8c5d82d5b082
agungardi5-spec <agungardi5@gmail.com>
2026-09-15 20:46:51 +0700

    Update service worker cache version to v4

--- www/sw.js
+++ www/sw.js
@@ -1,4 +1,4 @@
-const CACHE='accounting-hub-v3';
+const CACHE='accounting-hub-v4';
 const ASSETS=['./','./index.html','./manifest.json','./data/accounting.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
